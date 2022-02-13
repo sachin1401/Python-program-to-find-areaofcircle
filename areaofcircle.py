f3c3bfa6f72f97e07a8b97e34099113d055b4ba4
Sachin Patel <85048630+sachin1401@users.noreply.github.com>
2022-02-13 14:52:05 +0530

    Add files via upload

---
+++ areaofcircle.py
@@ -0,0 +1,14 @@
+# Python Program for Program to find area of a circle
+
+# here we use math function
+from cmath import pi #here we import pi 
+import math
+
+# enter the value of r
+r=int(input('Enter the value of r: '))
+
+# formula of area of a circle
+Area=pi*r**2
+
+# output
+print(Area)
---
+++ areaofcircle2.py
@@ -0,0 +1,13 @@
+# area of circle
+
+# eneter r
+r=int(input("Enter the value of r:"))
+
+# enter pi
+pi=float(input("Enter the value of pi:"))
+
+# formula of area of circle
+Area=pi*r**2
+
+# output
+print("the value of area:",Area)
